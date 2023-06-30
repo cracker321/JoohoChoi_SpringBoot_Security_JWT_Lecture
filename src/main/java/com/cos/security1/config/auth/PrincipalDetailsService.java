c7de6d7ee361f8e319543ba43df44f9ea15a3480
yujong <yujong4170@gmail.com>
2023-06-30 18:41:52 +0900

    yujong

--- src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
@@ -14,7 +14,8 @@ import org.springframework.stereotype.Service;
 
 //< 스프링 시큐리티의 내장 인터페이스 UserDetailsService >
 
-//- 스프링 시큐리티에서 제공하는 내장 인터페이스로, '사용자 정보를 가져오는 역할'을 함.
+//- 스프링 시큐리티에서 제공하는 내장 인터페이스로,
+//  '외부 클래스에서 내장 메소드 loadUserByUsername을 호출할 때, 그 때 파라미터로 입력된 사용자 정보를 DB로부터 가져오는 역할'을 함.
 //- 이 내장 인터페이스를 사욯하기 위해서는, 그 구현하는 클래스(여기서는 '클래스 PrincipalDetailsService') 내부에서,
 //  '이 내장 인터페이스의 내장 메소드 loadUserByUsername'을 반드시 그 오버라이딩해 가져와서 재정의해줘야 함!!
 
@@ -27,6 +28,7 @@ import org.springframework.stereotype.Service;
 //  '사용자의 식별 정보', '비밀번호', '권한' 등을 담고 있음.
 //- 사용자 인증 과정(로그인 과정)에서 인증된 사용자의 정보를 나타내는 객체로 사용됨.
 //  스프링 시큐리티는 이 객체를 통해 사용자를 인증하고 권한을 부여함.
+
 
 //--------------------------------------------------------------------------------------------
 
@@ -48,7 +50,8 @@ public class PrincipalDetailsService implements UserDetailsService {
 
     //< 스프링 시큐리티의 내장 인터페이스 UserDetailsService의 내장 메소드 loadUserByUsername >
 
-    //- '스프링 시큐리티 내장 인터페이스 UserDetailService'를 구현하려면, 반드시 오버라이딩해야 하는 메소드임.
+    //- '외부 클래스에서 내장 메소드 loadUserByUsername을 호출할 때, 그 때 파라미터로 입력된 사용자 정보를 DB로부터 가져오는 역할'을 함.
+    //- '스프링 시큐리티 내장 인터페이스 UserDetailService'를 구현하려면, 반드시 오버라이딩해야 하는 그 내장 메소드임.
     //- 스프링 시큐리티는 이 내장 메소드 loadUserByUsername을 통해 사용자 인증 정보를 얻음.
     //  '사용자 이름 username'을 기반으로 사용자 정보를 가져오는 역할을 수행함.
     //- 스프링시큐리티의 인증 프로세스 과정에서 이 메소드 loadUserByUsrename이 외부 클래스에서 호출되어 이 메소드가 사용될 때,
@@ -62,15 +65,17 @@ public class PrincipalDetailsService implements UserDetailsService {
         //순서 1) '해당 사용자 User 객체'의 정보를 db로부터 조회해서 가져옴.
         User userEntity = userRepository.findByUsername(username);
 
-        //순서 2)-1 사용자 정보 확인: db로부터 가져온 사용자 정보가 null이 아닌 경우
-        //- null이 아닌 경우, 'PrincipalDetails 객체'를 '생성'하여 '반환'함.
-        //
+        //순서 2)-1 사용자 정보 확인: db에 해당 사용자가 있어서, 그 사용자(사용자 정보)를 db로부터 가져온 경우
+        //- db로부터 조회해서 가져온 사용자 User 객체의 정보가 null이 아닌 경우, 'PrincipalDetails 객체'를 '생성'하여 '반환'함.
+        //- 'PrincipalDetails 객체'는, '사용자 인증 과정(로그인 과정)'에서 인증된 사용자의 정보를 나타내는 객체로 사용됨.
+        //  스프링 시큐리티는 이 객체를 통해 사용자를 인증하고 권한을 부여함.
         if(userEntity != null){
 
             return new PrincipalDetails(userEntity);
         }
 
-        //순서 2)-2
+        //순서 2)-2 사용자 정보 확인: db에 해당 사용자가 없어서, 그 사용자(사용자 정보)를 db로부터 가져올 것이 없는 경우
+        //- db에서 해당 사용자(사용자 정보)를 찾지 못한 경우, null을 반환함.
         else
             return null;
 
